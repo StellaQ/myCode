00b6adf742c7a3b6c14e52c6db6443f304460a1d
StellaQ <845521178@qq.com>
2017-07-11 16:04:40 +0800

    ajax

--- baidu-ife/2015spring/task2/util3.js
+++ baidu-ife/2015spring/task2/util3.js
@@ -43,7 +43,33 @@ function getCookie (cookieName) {
 // 学习Ajax，并尝试自己封装一个Ajax方法。实现如下方法：
 
 function ajax(url, options) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            options.onsuccess(xhr.responseText, xhr);
+        else {
+            options.onfail(xhr.responseText, xhr);
+        };
+    };
+
+    var type = options.type ? options.type.toUpperCase() : 'GET';
+    var data = options.data ? options.data : {};
+    var params = '';
+    for (key in data) {
+        params += encodeURIComponent(key) + '=' + encodeURIComponent(data.key) + '&';
+    };
+    params = params.slice(0, -1);
+
+    if (type == 'GET') {
+        url += '?' + params;
+        xhr.open(type, url, true);
+        xhr.send(null);
+    } else if (type == 'POST') {
+        xhr.open(type, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(params);
+    };
 }
 
 // 使用示例：
@@ -61,7 +87,6 @@ function ajax(url, options) {
 // );
 
 // options是一个对象，里面可以包括的参数为：
-
 // type: post或者get，可以有一个默认值
 // data: 发送的数据，为一个键值对象或者为一个用&连接的赋值字符串
 // onsuccess: 成功时的调用函数
