1c6e3422f986516c394cc7baee225244390a4050
StellaQ <845521178@qq.com>
2017-07-18 11:31:51 +0800

    task2-003

--- baidu-ife/2015spring/task2/003/1.js
+++ baidu-ife/2015spring/task2/003/1.js
@@ -3,7 +3,7 @@ var intervalId = null;
 
 // 页面进入后开始间歇调用
 window.addEventListener('focus', function(){
-    intervalId = setInterval(function(){ loop(false) }, 1500);
+    intervalId = setInterval(function(){ imageLoop(true, true) }, 1500);
 }, false);
 // 页面离开时停止间歇调用
 window.addEventListener('blur', function(){
@@ -11,8 +11,6 @@ window.addEventListener('blur', function(){
 }, false);
 
 window.onload = function () {
-    // moveMent($('#imageList'), parseInt(getStyle($('#imageList')).left), true);
-
     $.on('.config [name=order]', 'change', listener1);
 };
 function listener1 () {
@@ -20,26 +18,25 @@ function listener1 () {
     console.log(this.value);
 
 };
-function loop (forward) {
+function imageLoop (forward, loop) {
 
     var start = parseInt(getStyle($('#imageList')).left),      // 当前左偏移值
         pace = parseInt(getStyle($('#imageList li')).width),   // 600px -> 600
         num = $('#imageList').getElementsByTagName('li').length, // 图片个数
         boundary = -pace*(num-1);
 
-    if (start == boundary && forward) {
+    if (start == boundary && forward && loop) {
         start = 0;
     };
-    if (start == 0 && !forward) {
+    if (start == 0 && !forward && loop) {
         start = boundary;
     };
 
-    moveMent($('#imageList'), start, forward);
+    moveMent($('#imageList'), start, pace, forward);
 
 };
-function moveMent (element, start, forward) {
-    var pace = parseInt(getStyle($('#imageList li')).width),   // 600px -> 600
-        counter = 10,                                          // 每一幅图片切换移动次数
+function moveMent (element, start, pace, forward) {
+    var counter = 10,                                          // 每一幅图片切换移动次数
         speed = forward ? (-pace/counter) : (pace/counter);    // 算出正负速率
 
     var intervalId = null;
