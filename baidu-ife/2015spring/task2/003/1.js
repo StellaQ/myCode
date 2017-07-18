c2c78e30f5c8a94d81f1d4a526565c73705add7e
StellaQ <845521178@qq.com>
2017-07-18 11:09:04 +0800

    task2-003

--- baidu-ife/2015spring/task2/003/1.js
+++ baidu-ife/2015spring/task2/003/1.js
@@ -1,39 +1,49 @@
+
+var intervalId = null;
+
+// 页面进入后开始间歇调用
+window.addEventListener('focus', function(){
+    intervalId = setInterval(function(){ loop(false) }, 1500);
+}, false);
+// 页面离开时停止间歇调用
+window.addEventListener('blur', function(){
+    clearInterval(intervalId);
+}, false);
+
 window.onload = function () {
     // moveMent($('#imageList'), parseInt(getStyle($('#imageList')).left), true);
-    imageLoop(false);
 
-    $.on('.config [name=order]', 'click', listener1);
+    $.on('.config [name=order]', 'change', listener1);
 };
 function listener1 () {
+    console.log('good');
     console.log(this.value);
-    // console.log('im here');
-    // clearInterval(intervalId);
+
 };
-var intervalId = null;
-function imageLoop (forward) {
+function loop (forward) {
 
-    intervalId = setInterval(function(){loop()}, 1500);
+    var start = parseInt(getStyle($('#imageList')).left),      // 当前左偏移值
+        pace = parseInt(getStyle($('#imageList li')).width),   // 600px -> 600
+        num = $('#imageList').getElementsByTagName('li').length, // 图片个数
+        boundary = -pace*(num-1);
 
-    function loop () {
-
-        var start = parseInt(getStyle($('#imageList')).left);
-        if (start == -2400 && forward) {
-            start = 0;
-        };
-        if (start == 0 && !forward) {
-            start = -2400;
-        };
-
-        moveMent($('#imageList'), start, forward);
+    if (start == boundary && forward) {
+        start = 0;
     };
+    if (start == 0 && !forward) {
+        start = boundary;
+    };
+
+    moveMent($('#imageList'), start, forward);
+
 };
 function moveMent (element, start, forward) {
-    var pace = parseInt(getStyle($('#imageList li')).width),
-        counter = 10,
-        speed = forward ? (-pace/counter) : (pace/counter);
+    var pace = parseInt(getStyle($('#imageList li')).width),   // 600px -> 600
+        counter = 10,                                          // 每一幅图片切换移动次数
+        speed = forward ? (-pace/counter) : (pace/counter);    // 算出正负速率
 
     var intervalId = null;
-    intervalId = setInterval(function(){move()}, 100);
+    intervalId = setInterval(function(){ move() }, 100);
 
     function move () {
         counter--;
