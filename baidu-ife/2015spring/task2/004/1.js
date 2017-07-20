19a316514c218f527859ab054aaac5aea93d6f27
StellaQ <845521178@qq.com>
2017-07-20 15:29:23 +0800

    task2-004

--- baidu-ife/2015spring/task2/004/1.js
+++ baidu-ife/2015spring/task2/004/1.js
@@ -1,22 +1,54 @@
 var searchResult = ['djfjf', 'ffrrf', 'frferer', 'frfrfrfr'];
+var i = -1;
+
 window.onload = function () {
-    $.on('[name=search]', 'keypress', listener1);
+    $.on('[name=search]', 'keydown', listener1);
     $.delegate('.result', 'li', 'click', listener2);
 };
 function listener1 (ev) {
-    var charCode = getCharCode(ev);
-    if (charCode == 13) {  //13 == enter
-        var str = $('[name=search]').value;
-        if (str == '') { return }
-        searchText(str);
+    var charCode = ev.keyCode;
+    // console.log(charCode);
+    if (!(charCode == 40 || charCode == 38 || charCode == 13)) {
+        // console.log('not target keyCode');
+        return;
     };
+    if (charCode == 13) {  //13 == enter
+        // console.log($('.result').style.display);
+        if ($('.result').style.display !== 'block') {
+            var str = $('[name=search]').value;
+            if (str == '') { return }
+            searchText(str);
+        } else {
+            console.log(i);
+            $('[name=search]').value = $('.result').getElementsByTagName('li')[i].innerHTML;
+            $('.result').style.display = 'none';
+        }
+
+    };
+    if (charCode == 40) {
+        i++;
+        if (i > 3) {
+            i = 3;
+        };
+        // console.log(i);
+        tabLi(i);
+    } else if (charCode == 38) {
+        i--;
+        if (i < 0) {
+            i = 0;
+        };
+        // console.log(i);
+        tabLi(i);
+    };
+    // console.log();
 }
-function getCharCode (event) {
-    if (typeof event.charCode == 'number') {
-        return event.charCode;
-    } else {
-        return event.keyCode;
-    }
+function tabLi (index) {
+
+    var arrLi = $('.result').getElementsByTagName('li');
+    for (var i = 0; i < arrLi.length; i++) {
+        removeClass(arrLi[i], 'active');
+    };
+    addClass(arrLi[index], 'active');
 }
 function searchText (str) {
     // ajax
