5e766cd921ed6a8624b0feb0a206615f51b35605
StellaQ <845521178@qq.com>
2017-07-21 17:00:14 +0800

    task2-005

--- baidu-ife/2015spring/task2/005/1.js
+++ baidu-ife/2015spring/task2/005/1.js
@@ -1,14 +1,27 @@
 window.onload = function () {
-    $.on('.listOne', 'dragover', listenerDragOver);
-    $.on('.listOne', 'drop', listenerDrop);
+
+    $.delegate('.listOne', 'li', 'dragstart', listenerDragStart);
+    $.delegate('.listOne', 'li', 'dragend', listenerDragEnd);
+
     $.delegate('.listTwo', 'li', 'dragstart', listenerDragStart);
     $.delegate('.listTwo', 'li', 'dragend', listenerDragEnd);
 
     $.on('.listTwo', 'dragover', listenerDragOver);
     $.on('.listTwo', 'drop', listenerDrop);
-    $.delegate('.listOne', 'li', 'dragstart', listenerDragStart);
-    $.delegate('.listOne', 'li', 'dragend', listenerDragEnd);
+
+    $.on('.listOne', 'dragover', listenerDragOver);
+    $.on('.listOne', 'drop', listenerDrop);
+
 };
+function listenerDragStart (ev) {
+    ev.dataTransfer.effectAllowed = 'move';
+    ev.dataTransfer.setData('text', ev.target.innerHTML);
+    ev.dataTransfer.setData('el', ev.target);
+    ev.target.style.opacity = '0.5';
+}
+function listenerDragEnd (ev) {
+    ev.target.style.opacity = '1';
+}
 function listenerDragOver (ev) {
     preventDefault(ev);
     ev.dropEffect = 'move';
@@ -21,15 +34,6 @@ function listenerDrop (ev) {
     var el = ev.dataTransfer.getData('el');
     console.log(el);
     console.log(el.parentNode);
-}
-function listenerDragStart (ev) {
-    ev.dataTransfer.effectAllowed = 'move';
-    ev.dataTransfer.setData('text', ev.target.innerHTML);
-    ev.dataTransfer.setData('el', ev.target);
-    ev.target.style.opacity = '0.5';
-}
-function listenerDragEnd (ev) {
-    ev.target.style.opacity = '1';
 }
 function preventDefault (event) {  // javascript高级程序设计13.3.3跨浏览器的事件对象
     if (event.preventDefault) {
