bfef500788a793776c87b47817829937b1d9e9d0
StellaQ <845521178@qq.com>
2017-07-28 15:47:07 +0800

    task3

--- baidu-ife/2015spring/task3/1.js
+++ baidu-ife/2015spring/task3/1.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-    $('h6').click(function(){
-        $('.list-task').removeClass('list-active');
-        $('.list-task li').removeClass('task-active');
-        $(this).children('.list-task').addClass('list-active');
-        $(this).children('.list-task').children().first().addClass('task-active');
-    });
-    $('.list-task li').click(function(){
-        event.stopPropagation();
-        $('.list-task li').removeClass('task-active');
-        $(this).addClass('task-active');
-    });
+    $('aside.list').delegate('h6', 'click', listener1);
+    $('aside.list').delegate('.list-task li', 'click', listener2);
+    $('aside.list').delegate('h6 div a', 'click', listener3);
 });
+function listener1 () {
+    $('.list-task').removeClass('list-active');
+    $('.list-task li').removeClass('task-active');
+    $(this).children('.list-task').addClass('list-active');
+    $(this).children('.list-task').children().first().addClass('task-active');
+};
+function listener2 (event) {
+    event.stopPropagation();
+    $('.list-task li').removeClass('task-active');
+    $(this).addClass('task-active');
+};
+function listener3 (event) {
+    event.stopPropagation();
+    console.log('delete');
+}
