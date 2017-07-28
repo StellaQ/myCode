a6b6e6611c6177b73a78c26c3778ef0ee9f8c48f
StellaQ <845521178@qq.com>
2017-07-28 14:09:43 +0800

    task3

--- baidu-ife/2015spring/task3/1.js
+++ baidu-ife/2015spring/task3/1.js
@@ -1,3 +1,13 @@
-window.onload = function () {
-
-};
+$(document).ready(function(){
+    $('h6').click(function(){
+        $('.list-task').removeClass('list-active');
+        $('.list-task li').removeClass('task-active');
+        $(this).children('.list-task').addClass('list-active');
+        $(this).children('.list-task').children().first().addClass('task-active');
+    });
+    $('.list-task li').click(function(){
+        event.stopPropagation();
+        $('.list-task li').removeClass('task-active');
+        $(this).addClass('task-active');
+    });
+});
