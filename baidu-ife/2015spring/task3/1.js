a01d2ac576d0c542ff7ddbe141e31a3cfad72119
StellaQ <845521178@qq.com>
2017-08-01 13:43:38 +0800

    task3

--- baidu-ife/2015spring/task3/1.js
+++ baidu-ife/2015spring/task3/1.js
@@ -1,5 +1,7 @@
 var deleteObj = {};
 $(document).ready(function(){
+    init();
+
     $('aside.list').delegate('h6', 'click', listener1);
     $('aside.list').delegate('.list-task li', 'click', listener2);
     $('aside.list').delegate('h6 div a', 'click', listener3);
@@ -26,7 +28,7 @@ function listener2 (event) {
 }
 function listener3 (event) {
     event.stopPropagation();
-    // $('.alert-panel').removeClass('animation-show');
+    // $('.alert-panel').removeClass('animation-show');？？
     var name = $(this).prev().text();
     var id = $(this).parent().parent().data('id');
     deleteObj.name = name;
@@ -37,6 +39,49 @@ function listener3 (event) {
 }
 function deleteTask (name, id) {
 }
+function init () {
+    var str = '<h4>所有任务(<span>11</span>)</h4><h5>分类列表</h5>';
+    for (var i = 0; i < task.length; i++) {
+        str += renderTask(task[i]);
+    };
+    $('.list-container').html(str);
+    $('.list-container h6:first .list-task').addClass('list-active');
+    $('.list-container h6:first .list-task li:first').addClass('task-active');
+}
+function renderTask (obj) {
+    var str = '',
+        liststr = '',
+        arr = obj.list,
+        len = arr.length;
+
+    if (len > 0) {
+        for (var i = 0; i < arr.length; i++) {
+            liststr += '<li class="clearfix"><span class="icon icon-book fl mr5"></span>'+
+            '<p class="fl">'+ arr[i].name +'<span>('+ arr[i].num +')</span></p>'+
+            '<a href="javascript:;" class="icon icon-delete fr"></a></li>'
+        }
+    };
+    str = '<h6 class="clearfix" data-id='+ obj.id
+    +'><div><span class="icon icon-star fl mr5"></span><p class="fl">'+
+    obj.name +'<span>('+ obj.num +
+    ')</span></p><a href="javascript:;" class="icon icon-delete fr"></a></div>'+
+    '<ul class="list-task fl">'+ liststr + '</ul></h6>';
+    return str;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ baidu-ife/2015spring/task3/data.js
@@ -0,0 +1,41 @@
+var task = [
+    {
+        id: 'h6-0',
+        name: '百度IFE项目',
+        num: 10,
+        list: [
+            {name: 'task1',
+            num: 6},
+            {name: 'task2',
+            num: 4}
+        ]
+    },
+    {
+        id: 'h6-1',
+        name: '毕业设计',
+        num: 1,
+        list: [
+            {name: 'taska',
+            num: 1}
+        ]
+    },
+    {
+        id: 'h6-2',
+        name: '社团活动',
+        num: 0,
+        list: []
+    },
+    {
+        id: 'h6-3',
+        name: '家庭生活',
+        num: 0,
+        list: []
+    },
+    {
+        id: 'h6-4',
+        name: '默认分类',
+        num: 0,
+        list: []
+    }
+
+]
