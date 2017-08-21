f6355d85fa4a47197d675e1b8fb9fd6dfee5e890
StellaQ <845521178@qq.com>
2017-08-21 19:47:56 +0800

    mob_yangfan

--- baidu-ife/2015summer/task1mob/public/js/4.js
+++ baidu-ife/2015summer/task1mob/public/js/4.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    // $('.item').on('click', function(){
     $('.item').on('tap', function(){
         if ($('.calContainer').css('display') == 'none') {
             $('.calContainer').show();
@@ -9,51 +8,92 @@ $(document).ready(function(){
         $('.output').children().eq(0).attr('class', className + ' fl');
         $('.output .type').html(text);
 
-        $('.output .total').html('¥0');
-        current_num = 0;
+        $('.btnReset').trigger('tap');
     });
     $('.btnCancel').on('tap', function(){
         $('.calContainer').hide();
 
+        $('.btnReset').trigger('tap');
+    });
+
+    $('.btnReset').on('tap', function(){
         $('.output .total').html('¥0');
         current_num = 0;
+        prev_arr = [];
     });
+
     $('.btnNum').on('tap', function(){
-        cal($(this).text());
-        // console.log(current_num);
+        calCurrent($(this).text());
     });
     $('.btnDot').on('tap', function(){
         current_dot = true;
     });
-    $('.btnReset').on('tap', function(){
-        $('.output .total').html('¥0');
+
+    $('.btnAdd').on('tap', function(){
+        prev_arr.push({num: current_num, type: '+'});
         current_num = 0;
     });
-    $('.btnAdd').on('tap', function(){
-        btn_add = true;
-        prev_num.push({num: current_num, type: '+'});
-        console.log(prev_num);
+    $('.btnMinus').on('tap', function(){
+        prev_arr.push({num: current_num, type: '-'});
         current_num = 0;
     });
     $('.btnEqual').on('tap', function(){
-        console.log(prev_num);
-        console.log(current_num);
+        is_end = true;
+        getTotal(prev_arr, current_num);
     })
 });
-var current_num = 0;
-var current_dot = false;
-var btn_add = false;
-var prev_num = [];
-function cal (num) {
+
+var current_num = 0,
+    prev_arr = [],
+    current_dot = false,
+    is_end = false;
+
+function calCurrent (num) {
+    // 上一次=计算总数后再按btnNum，开始重新计数
+    if (is_end) {
+        $('.btnReset').trigger('tap');
+
+        is_end = false;
+    };
     if (!current_dot) {
         var arr = current_num.toString().split('.');
-        if (arr.length > 1 && arr[1].length == 2) {
-            return;
-        };
+        // 防止1.222超2位小数的出现
+        if (arr.length > 1 && arr[1].length == 2) { return; };
+
         current_num = Number(current_num + num);
     } else{
-        current_num = Number(current_num + '.' + num);
         current_dot = false;
+
+        var arr = (current_num + '.' + num).split('.');
+        // 防止类似1.2.2格式出现
+        if (arr.length > 2) { return };
+
+        current_num = Number(current_num + '.' + num);
     };
+
     $('.output .total').html('¥' + current_num);
+}
+function getTotal (arr, num){
+    // 考虑到按下1个数字后直接算总数的情况
+    if (arr.length == 0) { return $('.output .total').html('¥' + num); };
+
+    var total = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (i == 0) {
+            total += arr[i].num;
+        } else {
+            if (arr[i-1].type == '+') {
+                total += arr[i].num;
+            } else if (arr[i-1].type == '-') {
+                total -= arr[i].num;
+            }
+        };
+    };
+    if (arr[arr.length-1].type == '+') {
+        total += num;
+    } else if (arr[arr.length-1].type == '-') {
+        total -= num;
+    };
+    // console.log(total);
+    $('.output .total').html('¥' + total);
 }
