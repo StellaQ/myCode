bf6e1ad5cfa6aee59142cdb4879fa98f5482775e
StellaQ <845521178@qq.com>
2017-08-21 16:59:52 +0800

    mob_yangfan

--- baidu-ife/2015summer/task1mob/public/js/4.js
+++ baidu-ife/2015summer/task1mob/public/js/4.js
@@ -7,25 +7,53 @@ $(document).ready(function(){
         var className = $(this).children().eq(0).attr('class');
         var text = $(this).children().eq(1).text();
         $('.output').children().eq(0).attr('class', className + ' fl');
-        $('.output').children('.type').html(text);
+        $('.output .type').html(text);
+
+        $('.output .total').html('¥0');
+        current_num = 0;
     });
     $('.btnCancel').on('tap', function(){
         $('.calContainer').hide();
+
+        $('.output .total').html('¥0');
+        current_num = 0;
     });
     $('.btnNum').on('tap', function(){
         cal($(this).text());
-        console.log(current_num);
-        current_i++;
+        // console.log(current_num);
     });
     $('.btnDot').on('tap', function(){
         current_dot = true;
     });
+    $('.btnReset').on('tap', function(){
+        $('.output .total').html('¥0');
+        current_num = 0;
+    });
+    $('.btnAdd').on('tap', function(){
+        btn_add = true;
+        prev_num.push({num: current_num, type: '+'});
+        console.log(prev_num);
+        current_num = 0;
+    });
+    $('.btnEqual').on('tap', function(){
+        console.log(prev_num);
+        console.log(current_num);
+    })
 });
 var current_num = 0;
-var current_i = 0;
 var current_dot = false;
+var btn_add = false;
+var prev_num = [];
 function cal (num) {
-    current_num = current_i == 0
-    ? (current_num * 0 + Number(num))
-    : (current_num * 10 + Number(num));
+    if (!current_dot) {
+        var arr = current_num.toString().split('.');
+        if (arr.length > 1 && arr[1].length == 2) {
+            return;
+        };
+        current_num = Number(current_num + num);
+    } else{
+        current_num = Number(current_num + '.' + num);
+        current_dot = false;
+    };
+    $('.output .total').html('¥' + current_num);
 }
